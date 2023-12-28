1275b8e1428d8123055c55c346c49fd6c370672c
Ilchenko Oleh <deckart212@gmail.com>
2023-12-28 06:24:05 +0200

    add user registration

--- server/app.ts
+++ server/app.ts
@@ -4,17 +4,25 @@ export const app = express();
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import { ErrorMiddleware } from './middleware/error';
+import userRouter from './routes/user.route';
 
+//body parser
 app.use(express.json({ limit: '50mb' }));
 
+//cookie parser
 app.use(cookieParser());
 
+cors;
 app.use(
   cors({
     origin: process.env.ORIGIN,
   })
 );
 
+//routes
+app.use('/api/v1', userRouter);
+
+//testing api
 app.get('/test', (req: Request, res: Response, next: NextFunction) => {
   res.status(200).json({
     success: true,
@@ -22,6 +30,7 @@ app.get('/test', (req: Request, res: Response, next: NextFunction) => {
   });
 });
 
+//unknown route
 app.all('*', (req: Request, res: Response, next: NextFunction) => {
   const err = new Error(`route ${req.originalUrl} not found`) as any;
   err.statusCode = 404;
--- server/utils/db.ts
+++ server/utils/db.ts
@@ -1,14 +1,14 @@
 import mongoose from 'mongoose';
 require('dotenv').config();
 
-const dbUrl: string = process.env.DB_URL || '';
+const dbUrl: string = process.env.DB_URI || '';
 
 const connectDB = async () => {
   try {
     (await mongoose.connect(dbUrl)).isObjectIdOrHexString((data: any) => {
       console.log(`Database connected with ${data.connection.host}`);
     });
-  } catch (error) {
+  } catch (error: any) {
     console.log(error.message);
     setTimeout(connectDB, 5000);
   }
