cbdd69a58cb3dd696799400d8417afdb19a21453
Ilchenko Oleh <deckart212@gmail.com>
2023-12-29 04:08:53 +0200

    add user social authentication and get user info

--- server/routes/user.route.ts
+++ server/routes/user.route.ts
@@ -1,9 +1,12 @@
 import express from 'express';
 import {
   activateUser,
+  getUserInfo,
   loginUser,
   logoutUser,
   registrationUser,
+  socialAuth,
+  updateAccessToken,
 } from '../controllers/user.controller';
 import { authorizeRoles, isAutheticated } from '../middleware/auth';
 
@@ -13,5 +16,8 @@ userRouter.post('/registration', registrationUser);
 userRouter.post('/activate-user', activateUser);
 userRouter.post('/login', loginUser);
 userRouter.get('/logout', isAutheticated, authorizeRoles('admin'), logoutUser);
+userRouter.get('/refresh', updateAccessToken);
+userRouter.get('/me', isAutheticated, getUserInfo);
+userRouter.post('/social-auth', socialAuth);
 
 export default userRouter;
