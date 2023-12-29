9bd62cfb736d72ca61c51b5c369f06a56a3d15de
Ilchenko Oleh <deckart212@gmail.com>
2023-12-29 03:21:06 +0200

    add user protected and auth middleware

--- server/routes/user.route.ts
+++ server/routes/user.route.ts
@@ -5,14 +5,13 @@ import {
   logoutUser,
   registrationUser,
 } from '../controllers/user.controller';
+import { authorizeRoles, isAutheticated } from '../middleware/auth';
+
 const userRouter = express.Router();
 
 userRouter.post('/registration', registrationUser);
-
 userRouter.post('/activate-user', activateUser);
-
 userRouter.post('/login', loginUser);
-
-userRouter.get('/logout', logoutUser);
+userRouter.get('/logout', isAutheticated, authorizeRoles('admin'), logoutUser);
 
 export default userRouter;
